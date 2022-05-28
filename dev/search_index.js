4b3b578cec28c10674d35a7eab0edb6444a6cbaf
Documenter.jl <documenter@juliadocs.github.io>
2022-05-28 08:38:43 +0000

    build based on 78a581a

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = GalerkinToolkit","category":"page"},{"location":"#GalerkinToolkit","page":"Home","title":"GalerkinToolkit","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for GalerkinToolkit.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [GalerkinToolkit]","category":"page"}]
+}
