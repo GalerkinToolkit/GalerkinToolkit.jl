e0c837e83e037e4bb672c59eea2f7d26fe3c8b7c
Francesc Verdugo <f.verdugo.rojano@vu.nl>
2024-01-25 13:37:06 +0100

    Implemented general LagrangianFEs

--- src/GalerkinToolkit.jl
+++ src/GalerkinToolkit.jl
@@ -84,26 +84,51 @@ function face_dim(a)
     reduce(vcat,map(d->face_dim(a,d),0:D))
 end
 
-struct UnitSimplex{D,Tv,Ti} <: GalerkinToolkitDataType
-    num_dims::Val{D}
-    real_type::Type{Tv}
-    int_type::Type{Ti}
-end
-
-struct UnitNCube{D,Tv,Ti} <: GalerkinToolkitDataType
-    num_dims::Val{D}
-    real_type::Type{Tv}
+struct ExtrusionPolytope{D,Tv,Ti} <: GalerkinToolkitDataType
+    extrusion::NTuple{D,Bool}
+    bounding_box::NTuple{2,SVector{D,Tv}}
     int_type::Type{Ti}
 end
 
 function unit_simplex(num_dims;real_type=Float64,int_type=Int)
     D = val_parameter(num_dims)
-    UnitSimplex(Val(D),real_type,int_type)
+    extrusion = ntuple(i->false,Val(D))
+    Tv = real_type
+    p0 = ntuple(i->zero(real_type),Val(D)) |> SVector{D,Tv}
+    p1 = ntuple(i->one(real_type),Val(D)) |> SVector{D,Tv}
+    bounding_box = (p0,p1)
+    ExtrusionPolytope(extrusion,bounding_box,int_type)
 end
 
 function unit_n_cube(num_dims;real_type=Float64,int_type=Int)
     D = val_parameter(num_dims)
-    UnitNCube(Val(D),real_type,int_type)
+    extrusion = ntuple(i->true,Val(D))
+    Tv = real_type
+    p0 = ntuple(i->zero(real_type),Val(D)) |> SVector{D,Tv}
+    p1 = ntuple(i->one(real_type),Val(D)) |> SVector{D,Tv}
+    bounding_box = (p0,p1)
+    ExtrusionPolytope(extrusion,bounding_box,int_type)
+end
+
+num_dims(p::ExtrusionPolytope{D}) where D = D
+is_axis_aligned(p::ExtrusionPolytope) = true
+is_simplex(geom::ExtrusionPolytope) = all(i->i==false,geom.extrusion)
+is_n_cube(geom::ExtrusionPolytope) = all(geom.extrusion)
+real_type(p::ExtrusionPolytope{D,Tv}) where {D,Tv} = Tv
+int_type(p::ExtrusionPolytope{D,Tv,Ti}) where {D,Tv,Ti} = Ti
+
+function is_unit_n_cube(geo)
+    is_n_cube(geo) && is_unitary(geo)
+end
+
+function is_unit_simplex(geo)
+    is_simplex(geo) && is_unitary(geo)
+end
+
+function is_unitary(geom)
+    ! is_axis_aligned(geom) && return false
+    my_bounding_box = bounding_box(geom)
+    all(i->i==0,first(my_bounding_box)) && all(i->i==1,last(my_bounding_box))
 end
 
 struct GenericCuadrature{A,B} <: GalerkinToolkitDataType
@@ -121,23 +146,8 @@ function quadrature(coordinates::Vector{SVector{D,T}},weights::Vector{T}) where 
     Cuadrature(coordinates,weights)
 end
 
-function default_quadrature(geo::UnitSimplex;
-    degree,
-    real_type=geo.real_type)
-    duffy_quadrature(geo;degree,real_type)
-end
-
-function default_quadrature(geo::UnitNCube;
-    degree,
-    real_type=geo.real_type)
-    degree_per_dir = ntuple(i->degree,Val(num_dims(geo)))
-    tensor_product_quadrature(geo;degree_per_dir,real_type)
-end
-
-function duffy_quadrature(geo::UnitSimplex;
-    degree,
-    real_type = geo.real_type,
-    )
+function duffy_quadrature(geo,degree)
+    @assert is_unit_simplex(geo)
     D = num_dims(geo)
     if D == 0
         x = zeros(SVector{0,real_type},1)
@@ -181,21 +191,21 @@ function duffy_quadrature(geo::UnitSimplex;
         a *= 0.5
     end
     m = prod(map(length,weights_per_dir))
-    w = zeros(real_type,m)
-    x = zeros(SVector{D,real_type},m)
+    Tv = real_type(geo)
+    w = zeros(Tv,m)
+    x = zeros(SVector{D,Tv},m)
     tensor_product!(identity,x,coords_per_dir)
     tensor_product!(prod,w,weights_per_dir)
     x .= duffy_map.(x)
     quadrature(x,w)
 end
 
-function tensor_product_quadrature(geo::UnitNCube;
-    degree_per_dir,
-    real_type = geo.real_type,
-    )
-
+function tensor_product_quadrature(geo,degree_per_dir)
+    @assert is_n_cube(geo)
+    @assert is_axis_aligned(geo)
+    my_bounding_box = bounding_box(geo)
     D = num_dims(geo)
-    limits_per_dir = ntuple(i->(0,1),Val(D))
+    limits_per_dir = ntuple(i->(my_bounding_box[1][i],my_bounding_box[2][i]),Val(D))
     n_per_dir = map(d->ceil(Int,(d+1)/2),degree_per_dir)
     function quadrature_1d(n,limits)
         x,w = FastGaussQuadrature.gausslegendre(n)
@@ -208,8 +218,9 @@ function tensor_product_quadrature(geo::UnitNCube;
     coords_per_dir = map(coordinates,quad_per_dir)
     weights_per_dir = map(weights,quad_per_dir)
     m = prod(map(length,weights_per_dir))
-    w = zeros(real_type,m)
-    x = zeros(SVector{D,real_type},m)
+    Tv = real_type(geo)
+    w = zeros(Tv,m)
+    x = zeros(SVector{D,Tv},m)
     tensor_product!(identity,x,coords_per_dir)
     tensor_product!(prod,w,weights_per_dir)
     quadrature(x,w)
@@ -226,45 +237,126 @@ function tensor_product!(f,result,values_per_dir)
     result
 end
 
+function repeat_per_dir(geo,a)
+    D = num_dims(geo)
+    ntuple(i->a,Val(D))
+end
+repeat_per_dir(geo,a::NTuple) = a
+
+function default_quadrature(geo,degree)
+    if is_n_cube(geo) && is_axis_aligned(geo)
+        D = num_dims(geo)
+        degree_per_dir = repeat_per_dir(geo,degree)
+        tensor_product_quadrature(geo,degree_per_dir)
+    elseif is_unit_simplex(geo)
+        duffy_quadrature(geo,degree)
+    else
+        error("Not implemented")
+    end
+end
+
 abstract type AbstractLagrangeFE <: GalerkinToolkitDataType end
 
-struct GenericLagrangeFE{A,B} <: AbstractLagrangeFE
+struct GenericLagrangeFE{A,B,C,D} <: AbstractLagrangeFE
     geometry::A
     order_per_dir::B
     space::Symbol
-end
-struct LagrangianUnitSimplex{D,Tv,Ti} <: AbstractLagrangeFE
-    geometry::UnitSimplex{D,Tv,Ti}
-    order_per_dir::NTuple{D,Ti}
-    space::Symbol
-end
-struct LagrangianUnitNCube{D,Tv,Ti} <: AbstractLagrangeFE
-    geometry::UnitNCube{D,Tv,Ti}
-    order_per_dir::NTuple{D,Ti}
-    space::Symbol
+    lib_to_user_nodes::C
+    major::Symbol
+    shape::D
 end
 
-function lagrangian_fe(geometry,order_per_dir,space)
-    GenericLagrangeFE(geometry,order_per_dir,space)
-end
-function lagrangian_fe(geometry::UnitSimplex,order_per_dir,space)
-    LagrangianUnitSimplex(geometry,order_per_dir,space)
-end
-function lagrangian_fe(geometry::UnitNCube,order_per_dir,space)
-    LagrangianUnitNCube(geometry,order_per_dir,space)
-end
+struct ScalarShape end
+const SCALAR_SHAPE = ScalarShape()
 
-function lagrangian_fe(geometry;order,space=default_space(geometry))
+#struct LagrangeFE{D,Tv,Ti} <: AbstractLagrangeFE
+#    geometry::ExtrusionPolytope{D,Tv,Ti}
+#    order_per_dir::NTuple{D,Ti}
+#    space::Symbol
+#    lib_to_user_nodes::Vector{Ti}
+#    major::Symbol
+#    shape::ScalarShape
+#end
+#
+#struct TensorValuedLagrangeFE{D,S,Tv,Ti} <: AbstractLagrangeFE
+#    geometry::ExtrusionPolytope{D,Tv,Ti}
+#    order_per_dir::NTuple{D,Ti}
+#    space::Symbol
+#    lib_to_user_nodes::Vector{Ti}
+#    major::Symbol
+#    shape::NTuple{S,Ti}
+#end
+
+lagrangian_fe(args...) = GenericLagrangeFE(args...)
+
+#function lagrangian_fe(
+#        geometry::ExtrusionPolytope{D,Tv,Ti},
+#        order_per_dir::NTuple{D,Ti},
+#        space::Symbol,
+#        lib_to_user_nodes::Vector{Ti},
+#        major::Symbol,
+#        shape::ScalarShape) where {D,Tv,Ti}
+#
+#    LagrangeFE(
+#               geometry,
+#               order_per_dir,
+#               space,
+#               lib_to_user_nodes,
+#               major,
+#               shape)
+#end
+#
+#function lagrangian_fe(
+#        geometry::ExtrusionPolytope{D,Tv,Ti},
+#        order_per_dir::NTuple{D,Ti},
+#        space::Symbol,
+#        lib_to_user_nodes::Vector{Ti},
+#        major::Symbol,
+#        shape::NTuple{S,Ti}) where {D,S,Tv,Ti}
+#
+#    TensorValuedLagrangeFE(
+#               geometry,
+#               order_per_dir,
+#               space,
+#               lib_to_user_nodes,
+#               major,
+#               shape)
+#end
+
+function lagrangian_fe(geometry,order;
+        space = default_space(geometry),
+        lib_to_user_nodes = int_type(geometry)[],
+        major = :component,
+        shape = SCALAR_SHAPE)
     D = num_dims(geometry)
-    order_per_dir = ntuple(i->order,Val(D))
-    lagrangian_fe(geometry,order_per_dir,space)
+    order_per_dir = repeat_per_dir(geometry,order)
+    lagrangian_fe(
+               geometry,
+               order_per_dir,
+               space,
+               lib_to_user_nodes,
+               major,
+               shape)
 end
 
-default_space(::UnitSimplex) = :P
-default_space(::UnitNCube) = :Q
+function default_space(geom)
+    if is_simplex(geom)
+        :P
+    elseif is_n_cube(geom)
+        :Q
+    else
+        error("Not implemented")
+    end
+end
 
-function tensor_product_lagrangian_fe(geometry::UnitNCube;order_per_dir,space=:Q)
-    lagrangian_fe(geometry,order_per_dir,space)
+function lib_to_user_nodes(fe::AbstractLagrangeFE)
+    if length(fe.lib_to_user_nodes) == 0
+        nnodes = num_nodes(fe)
+        Ti = int_type(geometry(fe))
+        collect(Ti.(1:nnodes))
+    else
+        fe.lib_to_user_nodes
+    end
 end
 
 function monomial_exponents(fe::AbstractLagrangeFE)
@@ -272,6 +364,7 @@ function monomial_exponents(fe::AbstractLagrangeFE)
 end
 
 function node_coordinates(fe::AbstractLagrangeFE)
+    @assert fe |> geometry |> is_unitary
     mexps = monomial_exponents(fe)
     node_coordinates_from_monomials_exponents(mexps,fe.order_per_dir,fe.geometry |> real_type)
 end
@@ -289,14 +382,13 @@ function node_coordinates_from_monomials_exponents(monomial_exponents,order_per_
 end
 
 function monomial_exponents_from_space(space,args...)
-    filter = if space == :Q
-        (e,o)->true
+    if space == :Q
+        monomial_exponents_from_filter((e,o)->true,args...)
     elseif space == :P
-        (e,o)->sum(e)<=maximum(o)
+        monomial_exponents_from_filter((e,o)->sum(e)<=maximum(o),args...)
     else
         error("Case not implemented (yet)")
     end
-    monomial_exponents_from_filter(filter,args...)
 end
 
 function monomial_exponents_from_filter(f,order_per_dir,int_type)
@@ -316,14 +408,90 @@ function monomial_exponents_from_filter(f,order_per_dir,int_type)
     result
 end
 
-#function boundary(::UnitSimplex{0})
-#    nothing
-#end
-#
-#function boundary(::UnitNCube{0})
-#    nothing
-#end
+function tensor_basis(fe::AbstractLagrangeFE)
+    Ti = fe |> geometry |> int_type
+    if fe.shape == SCALAR_SHAPE
+        return Ti(1)
+    else
+        cis = CartesianIndices(val_parameter(fe.shape))
+        l = prod(val_parameter(fe.shape))
+        init_tensor = SArray{Tuple{val_parameter(fe.shape)...},Ti}
+        cis_flat = cis[:]
+        return map(cis_flat) do ci
+            init_tensor(ntuple(j->cis[j]==ci ? 1 : 0 ,Val(l)))
+        end
+    end
+end
 
+function primal_basis(fe::AbstractLagrangeFE)
+    scalar_basis = map(e->(x-> prod(x.^e)),fe|>monomial_exponents)
+    if fe.shape == SCALAR_SHAPE
+        return scalar_basis
+    else
+        primal_nested = map(scalar_basis) do monomial
+            map(tensor_basis(fe))  do e
+                x -> monomial(x)*e
+            end
+        end
+        return reduce(vcat,primal_nested)
+    end
+end
+
+function dual_basis(fe::AbstractLagrangeFE)
+    node_coordinates_reffe = fe|>node_coordinates
+    scalar_basis = map(x->(f->f(x)),node_coordinates_reffe)
+    if fe.shape == SCALAR_SHAPE
+        return scalar_basis
+    else
+        if fe.major === :component
+            dual_nested = map(node_coordinates_reffe) do x
+                map(tensor_basis(fe)) do e
+                    f->inner(e,f(x))
+                end
+            end
+        elseif fe.major === :node
+            dual_nested = map(tensor_basis(fe)) do e
+                map(node_coordinates_reffe) do x
+                    f->inner(e,f(x))
+                end
+            end
+        else
+            error("Not Implemented")
+        end
+        return reduce(vcat,dual_nested)
+    end
+end
+
+inner(a,b) = sum(map(*,a,b))
+value(f,x) = f(x)
+
+function shape_functions(fe)
+    primal = primal_basis(fe)
+    dual = dual_basis(fe)
+    primal_t = permutedims(primal)
+    A = value.(dual,primal_t)
+    B = A\I
+    n = length(primal)
+    map(1:n) do i
+        Bi = view(B,:,i)
+        x->begin
+            primal_t_x = map(f->f(x),primal_t)
+            (primal_t_x*Bi)[1,1]#TODO
+        end
+    end
+end
+
+function tabulator(fe)
+    primal = primal_basis(fe)
+    dual = dual_basis(fe)
+    primal_t = permutedims(primal)
+    A = value.(dual,primal_t)
+    B = A\I
+    (f,x) -> begin
+        C = broadcast(f,primal_t,x)
+        C*B
+    end
+end
 
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -15,35 +15,66 @@ cube2 = gt.unit_n_cube(2)
 cube3 = gt.unit_n_cube(3)
 display(cube3)
 
-quad = gt.default_quadrature(spx0;degree=2)
-quad = gt.default_quadrature(spx1;degree=2)
-quad = gt.default_quadrature(spx2;degree=2)
-quad = gt.default_quadrature(spx3;degree=2)
-
-quad = gt.default_quadrature(cube0;degree=2)
-quad = gt.default_quadrature(cube1;degree=2)
-quad = gt.default_quadrature(cube2;degree=2)
-quad = gt.default_quadrature(cube3;degree=2)
-
-quad = gt.default_quadrature(cube1;degree=4,real_type=Float32)
-quad = gt.default_quadrature(spx1;degree=4,real_type=Float32)
-
 @show typeof(spx0)
+@show typeof(cube0)
+@show typeof(spx1)
+@show typeof(cube1)
 
-fe = gt.lagrangian_fe(spx0,order=1)
-fe = gt.lagrangian_fe(spx1,order=1)
-fe = gt.lagrangian_fe(spx2,order=1)
-fe = gt.lagrangian_fe(spx3,order=1)
+degree = 4
+quad = gt.default_quadrature(spx0,degree)
+quad = gt.default_quadrature(spx1,degree)
+quad = gt.default_quadrature(spx2,degree)
+quad = gt.default_quadrature(spx3,degree)
+
+quad = gt.default_quadrature(cube0,degree)
+quad = gt.default_quadrature(cube1,degree)
+quad = gt.default_quadrature(cube2,degree)
+quad = gt.default_quadrature(cube3,degree)
+
+
+order = 1
+fe = gt.lagrangian_fe(spx0,order)
+fe = gt.lagrangian_fe(spx1,order)
+fe = gt.lagrangian_fe(spx2,order)
+fe = gt.lagrangian_fe(spx3,order)
 display(fe)
 
-fe = gt.lagrangian_fe(cube0,order=1)
-fe = gt.lagrangian_fe(cube1,order=1)
-fe = gt.lagrangian_fe(cube2,order=1)
-fe = gt.lagrangian_fe(cube3,order=1)
+fe = gt.lagrangian_fe(cube0,order)
+fe = gt.lagrangian_fe(cube1,order)
+fe = gt.lagrangian_fe(cube2,order)
+fe = gt.lagrangian_fe(cube3,order)
 display(fe)
 
-fe = gt.lagrangian_fe(cube2,order=3)
+fe = gt.lagrangian_fe(cube0,order)
+fe = gt.lagrangian_fe(cube2,order)
 @show gt.node_coordinates(fe)
+
+spx2 = gt.unit_simplex(2)
+quad = gt.default_quadrature(spx2,degree)
+fe = gt.lagrangian_fe(spx2,order)
+funs = gt.shape_functions(fe)
+x = gt.coordinates(quad)
+B = broadcast(gt.value,permutedims(funs),x)
+display(B)
+tabulator = gt.tabulator(fe)
+A = tabulator(gt.value,x)
+@test A≈B
+x = gt.node_coordinates(fe)
+A = tabulator(gt.value,x)
+@show A
+
+fe = gt.lagrangian_fe(spx2,order;shape=(3,))
+funs = gt.shape_functions(fe)
+x = gt.coordinates(quad)
+B = broadcast(gt.value,permutedims(funs),x)
+display(B)
+tabulator = gt.tabulator(fe)
+A = tabulator(gt.value,x)
+@test A≈B
+x = gt.node_coordinates(fe)
+A = tabulator(gt.value,x)
+@show A
+
 
 #∂spx0 = gt.boundary(spx0)
 #∂spx0 = gt.boundary(spx1)
