36f860a2afb2f6114834f5aa4f06611464ddfdb6
Francesc Verdugo <f.verdugo.rojano@vu.nl>
2024-02-04 09:57:14 +0100

    Moving examples to a separate CI action

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,6 +4,4 @@ using Test
 
 @testset "Mesh interface" begin include("mesh_interface_tests.jl") end
 
-@testset "Iso-parametric Poisson" begin include("iso_param_poisson_tests.jl") end
-
 end # module
