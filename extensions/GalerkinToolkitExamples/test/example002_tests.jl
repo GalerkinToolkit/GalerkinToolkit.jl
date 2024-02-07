7c32c7e5d0631f2677276044123adb4b42dac0f8
Francesc Verdugo <f.verdugo.rojano@vu.nl>
2024-02-07 14:56:50 +0100

    Running example002 in parallel with a petsc solver

--- extensions/GalerkinToolkitExamples/test/example002_tests.jl
+++ extensions/GalerkinToolkitExamples/test/example002_tests.jl
@@ -1,46 +1,7 @@
 module Example002Tests
 
-import GalerkinToolkit as gk
-using GalerkinToolkitExamples: Example002
-using Test
-using PartitionedArrays
-using PetscCall
-
-tol = 1.0e-10
-
-params = Dict{Symbol,Any}()
-results = Example002.main(params)
-@test results[:eh1] < tol
-@test results[:el2] < tol
-
-params = Dict{Symbol,Any}()
-domain = (0,10,0,10)
-cells_per_dir = (20,20)
-parts_per_dir = (2,2)
-np = prod(parts_per_dir)
-parts = DebugArray(LinearIndices((np,)))
-ghost_layers = 0
-mesh = gk.cartesian_mesh(domain,cells_per_dir,parts_per_dir;parts,ghost_layers)
-params[:mesh] = mesh
-results = Example002.main(params)
-@test results[:eh1] < tol
-@test results[:el2] < tol
-
-options = "-pc_type gamg -ksp_type cg -ksp_error_if_not_converged true -ksp_converged_reason -ksp_rtol 1.0e-6"
-PetscCall.init(args=split(options))
-
-params = Dict{Symbol,Any}()
-domain = (0,10,0,10,0,10)
-cells_per_dir = (40,40,40)
-parts_per_dir = (2,2,2)
-np = prod(parts_per_dir)
-parts = DebugArray(LinearIndices((np,)))
-ghost_layers = 0
-mesh = gk.cartesian_mesh(domain,cells_per_dir,parts_per_dir;parts,ghost_layers)
-params[:mesh] = mesh
-params[:export_vtu] = false
-params[:solver] = Example002.ksp_solver()
-results = Example002.main(params)
-results = Example002.main(params)
+include("example002_defs.jl")
+with_debug(example002_tests_np_4)
+with_debug(example002_tests_np_8)
 
 end # module
---
+++ extensions/GalerkinToolkitExamples/test/mpi_array/runtests.jl
@@ -0,0 +1,9 @@
+
+module MPIArrayRunTests
+
+using Test
+using PartitionedArrays
+
+@testset "example002" begin include("example002_tests.jl") end
+
+end #module
