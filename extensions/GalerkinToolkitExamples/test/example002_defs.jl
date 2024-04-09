eed905c038c9356653f8c51db40696340d14e500
Jared Frazier <cscidev001@gmail.com>
2024-04-09 09:07:52 +0200

    periodic 2D puzzlepiece example002 tests fail

--- extensions/GalerkinToolkitExamples/test/example002_defs.jl
+++ extensions/GalerkinToolkitExamples/test/example002_defs.jl
@@ -7,8 +7,14 @@ using PetscCall
 using Metis
 using WriteVTK
 
+options = "-pc_type gamg -ksp_type cg -ksp_error_if_not_converged true -ksp_converged_reason -ksp_rtol 1.0e-6"
+PetscCall.init(args=split(options))
+
 function example002_tests_np_4(distribute)
     tol = 1.0e-8
+    # options = "-pc_type gamg -ksp_type cg -ksp_error_if_not_converged true -ksp_converged_reason -ksp_rtol 1.0e-6"
+    # PetscCall.init(args=split(options))
+
     params = Dict{Symbol,Any}()
     domain = (0,10,0,10)
     cells_per_dir = (20,20)
@@ -47,8 +53,6 @@ function example002_tests_np_4(distribute)
     @test results[:eh1] < tol
     @test results[:el2] < tol
 
-    options = "-pc_type gamg -ksp_type cg -ksp_error_if_not_converged true -ksp_converged_reason -ksp_rtol 1.0e-6"
-    PetscCall.init(args=split(options))
     params = Dict{Symbol,Any}()
     domain = (0,10,0,10,0,10)
     cells_per_dir = (40,40,40)
@@ -60,6 +64,109 @@ function example002_tests_np_4(distribute)
     params[:mesh] = mesh
     params[:export_vtu] = false
     params[:solver] = Example002.ksp_solver()
-    results = Example002.main(params)
-    results = Example002.main(params)
+    results = Example002.main(params) # TODO: why call twice?
+    results = Example002.main(params) # no need check convergence due to petsc args
 end
+
+function example002_advanced_tests_np_4(distribute)
+    tol = 1.0e-8
+    # options = "-pc_type gamg -ksp_type cg -ksp_error_if_not_converged true -ksp_converged_reason -ksp_rtol 1.0e-6"
+    # PetscCall.init(args=split(options))
+
+    # ## Coarse 2D pmesh
+    domain = (0, 10, 0, 10)
+    cells = (4, 4)
+    parts_per_dir = (2, 2)
+    nparts = prod(parts_per_dir)
+    parts = DebugArray(LinearIndices((nparts,)))
+    coarse_pmesh = gk.cartesian_mesh(
+        domain, cells;
+        parts_per_dir, parts,
+        partition_strategy=gk.partition_strategy(; ghost_layers=0))
+
+    ## Periodic 2D square tests 
+    # using default solver 
+    params = Dict{Symbol,Any}()
+
+    # load unit cell 
+    unit_cell_mesh_fpath = joinpath(
+        @__DIR__,
+        "..", # projectdir?
+        "..",
+        "..",
+        "assets",
+        "unit_cell_2D_periodic_square_geometry_triangular_refcell.msh")
+    unit_cell_mesh = gk.mesh_from_gmsh(unit_cell_mesh_fpath)
+
+    final_pmesh, _ = gk.two_level_mesh(coarse_pmesh, unit_cell_mesh)
+
+    params[:mesh] = final_pmesh 
+    results = Example002.main(params)
+    @test results[:eh1] < tol
+    @test results[:el2] < tol
+
+    # using kspsolver
+    params[:export_vtu] = false
+    params[:solver] = Example002.ksp_solver()
+    results = Example002.main(params)
+    results = Example002.main(params)
+
+    ## Periodic 2D puzzle tests 
+    # using default solver 
+    params = Dict{Symbol,Any}()
+
+    # load unit cell 
+    unit_cell_mesh_fpath = joinpath(
+        @__DIR__,
+        "..", # projectdir?
+        "..",
+        "..",
+        "assets",
+        "unit_cell_2D_periodic_puzzlepiece_geometry_triangular_refcell.msh")
+    unit_cell_mesh = gk.mesh_from_gmsh(unit_cell_mesh_fpath)
+
+    final_pmesh, _ = gk.two_level_mesh(coarse_pmesh, unit_cell_mesh)
+
+    params[:mesh] = final_pmesh 
+    results = Example002.main(params)
+    @test results[:eh1] < tol
+    @test results[:el2] < tol
+
+    # using kspsolver
+    params[:export_vtu] = false
+    params[:solver] = Example002.ksp_solver()
+    results = Example002.main(params)
+    results = Example002.main(params)
+
+    ## Coarse 3D pmesh
+    domain = (0, 10, 0, 10, 0, 10)
+    cells = (4, 4, 4)
+    parts_per_dir = (2, 2, 2)
+    nparts = prod(parts_per_dir)
+    parts = DebugArray(LinearIndices((nparts,)))
+    coarse_pmesh = gk.cartesian_mesh(
+        domain, cells;
+        parts_per_dir, parts,
+        partition_strategy=gk.partition_strategy(; ghost_layers=0))
+
+    ## Periodic 3D box 
+    params = Dict{Symbol,Any}()
+
+    unit_cell_mesh_fpath = joinpath(
+        @__DIR__,
+        "..", 
+        "..",
+        "..",
+        "assets",
+        "unit_cell_3D_periodic_box_geometry_triangular_refcell.msh")
+    unit_cell_mesh = gk.mesh_from_gmsh(unit_cell_mesh_fpath)
+
+    final_pmesh, _ = gk.two_level_mesh(coarse_pmesh, unit_cell_mesh)
+
+    params[:mesh] = final_pmesh 
+    results = Example002.main(params)
+    @test results[:eh1] < tol
+    @test results[:el2] < tol
+
+    ## Periodic 3D puzzlepiece
+end 
