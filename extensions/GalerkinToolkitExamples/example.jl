a0ad02d692d28393b22cd6ad32bddd4bec8d5deb
Jared Frazier <cscidev001@gmail.com>
2024-04-17 13:20:13 +0200

    3D puzzle piece parallel and sequential mesh passes tests

--- extensions/GalerkinToolkitExamples/example.jl
+++ extensions/GalerkinToolkitExamples/example.jl
@@ -25,11 +25,11 @@ function test_solver_periodic_2D_puzzle_piece_mesh()
     domain = (0,10,0,10)
     cells = (2,2)
     coarse_mesh = gk.cartesian_mesh(domain,cells)
-    mesh, = gk.two_level_mesh(coarse_mesh,fine_mesh)
-    gk.label_boundary_faces!(mesh; physical_name="boundary")
+    final_mesh, = gk.two_level_mesh(coarse_mesh,fine_mesh)
+    gk.label_boundary_faces!(final_mesh; physical_name="boundary")
 
     params = Dict{Symbol,Any}()
-    params[:mesh] = mesh
+    params[:mesh] = final_mesh
     params[:dirichlet_tags] = ["boundary"] 
     params[:example_path] = joinpath(@__DIR__, "output", "puzzle_piece_2D_serial_test")
     params[:export_vtu] = true 
@@ -71,6 +71,78 @@ function test_solver_periodic_2D_puzzle_piece_pmesh()
     params[:mesh] = final_pmesh 
     params[:dirichlet_tags] = ["boundary"]
     params[:example_path] = joinpath(@__DIR__, "output", "puzzle_piece_2D_np_4_test")
+    params[:export_vtu] = true 
+    results = Example002.main(params)
+    @test results[:eh1] < tol 
+    @test results[:el2] < tol
+end
+
+function test_solver_periodic_3D_puzzle_piece_mesh()
+    tol = 1.0e-8
+
+    # Load periodic fine (unit cell) mesh with triangular refcells 
+    unit_cell_mesh_fpath = joinpath(
+        @__DIR__,
+        "..",
+        "..",
+        "assets",
+        "unit_cell_3D_periodic_puzzlepiece_geometry_triangular_refcell.msh")
+    fine_mesh = gk.mesh_from_gmsh(unit_cell_mesh_fpath)
+
+    # test 4x4x4 coarse mesh 
+    coarse_domain = (0, 10, 0, 10, 0, 10)
+    coarse_mesh_dims = (4, 4, 4)
+    coarse_mesh = gk.cartesian_mesh(coarse_domain, coarse_mesh_dims)
+
+    # Construct mesh
+    final_mesh, = gk.two_level_mesh(coarse_mesh,fine_mesh)
+    gk.label_boundary_faces!(final_mesh; physical_name="boundary")
+
+    # Call solver
+    params = Dict{Symbol,Any}()
+    params[:mesh] = final_mesh
+    params[:dirichlet_tags] = ["boundary"] 
+    params[:example_path] = joinpath(@__DIR__, "output", "puzzle_piece_3D_serial_test")
+    params[:export_vtu] = true 
+    results = Example001.main(params)
+    @test results[:eh1] < tol
+    @test results[:el2] < tol
+
+end 
+
+function test_solver_periodic_3D_puzzle_piece_pmesh()
+
+    tol = 1.0e-8
+
+    # Load periodic fine (unit cell) mesh with triangular refcells 
+    unit_cell_mesh_fpath = joinpath(
+        @__DIR__,
+        "..",
+        "..",
+        "assets",
+        "unit_cell_3D_periodic_puzzlepiece_geometry_triangular_refcell.msh")
+    fine_mesh = gk.mesh_from_gmsh(unit_cell_mesh_fpath)
+
+    # Coarse 4x4x4 domain with 2x2x2 parts 
+    domain = (0, 10, 0, 10, 0, 10)
+    cells = (4, 4, 4)
+    parts_per_dir = (2, 2, 2)
+    nparts = prod(parts_per_dir)
+    parts = DebugArray(LinearIndices((nparts,)))
+    coarse_pmesh = gk.cartesian_mesh(
+        domain, cells;
+        parts_per_dir, parts,
+        partition_strategy=gk.partition_strategy(; ghost_layers=0))
+
+    # Construct final mesh
+    final_pmesh, _ = gk.two_level_mesh(coarse_pmesh, fine_mesh) 
+    gk.label_boundary_faces!(final_pmesh; physical_name="boundary")
+
+    # Call solver
+    params = Dict{Symbol,Any}()
+    params[:mesh] = final_pmesh 
+    params[:dirichlet_tags] = ["boundary"]
+    params[:example_path] = joinpath(@__DIR__, "output", "puzzle_piece_3D_np_4_test")
     params[:export_vtu] = true 
     results = Example002.main(params)
     @test results[:eh1] < tol 
@@ -148,7 +220,9 @@ end
 
 TMP.test_solver_periodic_2D_puzzle_piece_mesh()
 TMP.test_solver_periodic_2D_puzzle_piece_pmesh()
-# TODO: testing 3D puzzle piece mesh and pmesh
+
+TMP.test_solver_periodic_3D_puzzle_piece_mesh()
+TMP.test_solver_periodic_3D_puzzle_piece_pmesh()
 
 TMP.test_solver_periodic_3D_box_mesh()
 TMP.test_solver_periodic_3D_box_pmesh()
