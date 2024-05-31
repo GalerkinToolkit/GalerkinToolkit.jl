cea6682fbbe17a68fd170dfe1b95c5fda03a397f
Francesc Verdugo <f.verdugo.rojano@vu.nl>
2024-05-31 17:02:04 +0200

    Cleanup

--- src/assembly.jl
+++ src/assembly.jl
@@ -32,39 +32,38 @@ function assemble_vector_count(state)
         end
     end
     n = 0
-    function aux(glue,sface,field)
-        aux(glue,domain_glue_style(glue),sface,field)
+    function loop(glue,field)
+        loop(glue,domain_glue_style(glue),field)
     end
-    function aux(glue,style::InteriorGlue,sface,field)
+    function loop(glue,style::InteriorGlue,field)
         sface_to_face = target_face(glue)
-        face = sface_to_face[sface]
         dim = 1
         num_face_dofs = gk.num_face_dofs(space,dim)
         field_per_dim = (field,)
-        index = gk.index(;face,field_per_dim)
-        ndofs = num_face_dofs(index)
-        n += ndofs
-    end
-    function aux(glue,style::CoboundaryGlue,sface,field)
-        sface_to_faces, _ = target_face(glue)
-        dim = 1
-        num_face_dofs = gk.num_face_dofs(space,dim)
-        faces = sface_to_faces[sface]
-        for face in faces
-            field_per_dim = (field,)
+        for sface in 1:gk.num_faces(gk.domain(glue))
+            face = sface_to_face[sface]
             index = gk.index(;face,field_per_dim)
             ndofs = num_face_dofs(index)
             n += ndofs
         end
     end
-    for (domain_and_contribution,field_to_glue) in zip(contributions,glues)
-        domain, _ = domain_and_contribution
-        map(fields,field_to_glue) do field,glue
-            for face in 1:gk.num_faces(domain)
-                # TODO move this call outside the loop over faces
-                aux(glue,face,field)
+    function loop(glue,style::CoboundaryGlue,field)
+        sface_to_faces, _ = target_face(glue)
+        dim = 1
+        num_face_dofs = gk.num_face_dofs(space,dim)
+        field_per_dim = (field,)
+        for sface in 1:gk.num_faces(gk.domain(glue))
+            faces = sface_to_faces[sface]
+            for face in faces
+                index = gk.index(;face,field_per_dim)
+                ndofs = num_face_dofs(index)
+                n += ndofs
             end
         end
+    end
+    for (domain_and_contribution,field_to_glue) in zip(contributions,glues)
+        domain, _ = domain_and_contribution
+        map(loop,field_to_glue,fields)
     end
     (;n,glues,fields,state...)
 end
@@ -81,55 +80,56 @@ function assemble_vector_fill(state)
     contributions = gk.contributions(integral)
     num_fields = gk.num_fields(space)
     n = 0
-    function aux(glue,sface,field,contribution)
-        aux(glue,domain_glue_style(glue),sface,field,contribution)
+    function loop(glue,field,contribution)
+        loop(glue,domain_glue_style(glue),field,contribution)
     end
-    function aux(glue,style::InteriorGlue,sface,field,contribution)
+    function loop(glue,style::InteriorGlue,field,contribution)
         sface_to_face = target_face(glue)
-        face = sface_to_face[sface]
         field_per_dim = (field,)
         dim = 1
         num_face_dofs = gk.num_face_dofs(space,dim)
-        index = gk.index(;face,field_per_dim)
-        ndofs = num_face_dofs(index)
         dof_map = gk.dof_map(space,dim)
-        for dof in 1:ndofs
-            n += 1
-            dof_per_dim = (dof,)
-            index2 = gk.index(;face,dof_per_dim,field_per_dim)
-            I[n] = dof_map(index2)
-            index3 = gk.replace_face(index2,sface)
-            V[n] = gk.term(contribution)(index3)
-        end
-    end
-    function aux(glue,style::CoboundaryGlue,sface,field,contribution)
-        sface_to_faces, _ = target_face(glue)
-        dim =1
-        dof_map = gk.dof_map(space,dim)
-        num_face_dofs = gk.num_face_dofs(space,dim)
-        faces = sface_to_faces[sface]
-        field_per_dim = (field,)
-        for (face_around,face) in enumerate(faces)
-            face_around_per_dim = (face_around,)
+        for sface in 1:gk.num_faces(gk.domain(glue))
+            face = sface_to_face[sface]
             index = gk.index(;face,field_per_dim)
             ndofs = num_face_dofs(index)
             for dof in 1:ndofs
                 n += 1
                 dof_per_dim = (dof,)
-                index2 = gk.index(;face,dof_per_dim,field_per_dim,face_around_per_dim)
+                index2 = gk.index(;face,dof_per_dim,field_per_dim)
                 I[n] = dof_map(index2)
                 index3 = gk.replace_face(index2,sface)
                 V[n] = gk.term(contribution)(index3)
             end
         end
     end
+    function loop(glue,style::CoboundaryGlue,field,contribution)
+        sface_to_faces, _ = target_face(glue)
+        dim =1
+        dof_map = gk.dof_map(space,dim)
+        num_face_dofs = gk.num_face_dofs(space,dim)
+        field_per_dim = (field,)
+        for sface in 1:gk.num_faces(gk.domain(glue))
+            faces = sface_to_faces[sface]
+            for (face_around,face) in enumerate(faces)
+                face_around_per_dim = (face_around,)
+                index = gk.index(;face,field_per_dim)
+                ndofs = num_face_dofs(index)
+                for dof in 1:ndofs
+                    n += 1
+                    dof_per_dim = (dof,)
+                    index2 = gk.index(;face,dof_per_dim,field_per_dim,face_around_per_dim)
+                    I[n] = dof_map(index2)
+                    index3 = gk.replace_face(index2,sface)
+                    V[n] = gk.term(contribution)(index3)
+                end
+            end
+        end
+    end
     for (domain_and_contribution,field_to_glue) in zip(contributions,glues)
         domain, contribution = domain_and_contribution
         map(fields,field_to_glue) do field,glue
-            for face in 1:gk.num_faces(domain)
-                # TODO move this call outside the loop over faces
-                aux(glue,face,field,contribution)
-            end
+            loop(glue,field,contribution)
         end
     end
     state
