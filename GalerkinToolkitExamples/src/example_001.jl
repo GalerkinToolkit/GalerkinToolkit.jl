68a3a7c8246b4bdd0940361051954684d64d87aa
Francesc Verdugo <f.verdugo.rojano@vu.nl>
2024-06-15 17:14:50 +0200

    Using AD to create manufactured functions

--- GalerkinToolkitExamples/src/example_001.jl
+++ GalerkinToolkitExamples/src/example_001.jl
@@ -12,7 +12,6 @@ using LinearAlgebra
 using SparseArrays
 using PartitionedArrays
 using TimerOutputs
-using IterativeSolvers: cg!
 
 function main(params_in)
     params_default = default_params()
@@ -37,40 +36,52 @@ function main_automatic(params)
         Ω = gk.domain(mesh,physical_names=params[:domain_tags])
         Ωref = gk.reference_domain(Ω)
         Γdiri = gk.domain(mesh;face_dim=D-1,physical_names=params[:dirichlet_tags])
+        Γ = gk.domain(mesh;face_dim=D-1,physical_names=params[:neumann_tags])
+        Γref = gk.reference_domain(Γ)
     end
 
     # Geometrical map
-    ϕ = gk.domain_map(Ωref,Ω)
+    ϕ_Ωref_Ω = gk.domain_map(Ωref,Ω)
+    ϕ_Γref_Γ = gk.domain_map(Γref,Γ)
+    ϕ_Γref_Ωref = gk.domain_map(Γref,Ωref)
     function ∇(u,q)
-        J = ForwardDiff.jacobian(ϕ,q)
+        J = ForwardDiff.jacobian(ϕ_Ωref_Ω,q)
         g = ForwardDiff.gradient(u,q)
         J\g
     end
     function dV(q)
-        J = ForwardDiff.jacobian(ϕ,q)
+        J = ForwardDiff.jacobian(ϕ_Ωref_Ω,q)
         abs(det(J))
     end
+    function dS(q)
+        J = ForwardDiff.jacobian(ϕ_Γref_Γ,q)
+        Jt = transpose(J)
+        sqrt(det(Jt*J))
+    end
+    Δ(u,x) = tr(ForwardDiff.jacobian(y->ForwardDiff.gradient(u,y),x))
 
     # Manufactured functions
     u = gk.analytical_field(params[:u],Ω)
-    f = gk.analytical_field(params[:f],Ω)
+    f(x) = -gk.call(Δ,u,x) # TODO
 
     # Interpolation
     @timeit timer "interpolation" begin
         interpolation_degree = params[:interpolation_degree]
         V = gk.lagrange_space(Ωref,interpolation_degree;dirichlet_boundary=Γdiri)
         uh = gk.zero_field(Float64,V)
-        gk.interpolate_dirichlet!(u∘ϕ,uh)
+        gk.interpolate_dirichlet!(u∘ϕ_Ωref_Ω,uh)
     end
 
     # Integration
     integration_degree = params[:integration_degree]
     dΩref = gk.measure(Ωref,integration_degree)
-
+    dΓref = gk.measure(Γref,integration_degree)
 
     # Weak form
     a(u,v) = ∫( q->∇(u,q)⋅∇(v,q)*dV(q), dΩref)
-    l(v) = ∫( q->f(q)*dV(q), dΩref)
+    l(v) =
+        ∫( q->f(ϕ_Ωref_Ω(q))*v(q)*dV(q) , dΩref) #+
+        #∫( q->g(ϕ_Γref_Γ(q))*v(ϕ_Γref_Ωref(q))*dS(q) , dΓref)
 
     # FE assembly
     @timeit timer "assembly" begin
@@ -85,8 +96,8 @@ function main_automatic(params)
 
     # Error norms
     @timeit timer "error_norms" begin
-        eh(q) = u(ϕ(q)) - uh(q)
-        ∇eh(q) = ForwardDiff.gradient(u,ϕ(q)) - ∇(uh,q)
+        eh(q) = u(ϕ_Ωref_Ω(q)) - uh(q)
+        ∇eh(q) = ForwardDiff.gradient(u,ϕ_Ωref_Ω(q)) - ∇(uh,q)
         el2 = ∫( q->abs2(eh(q))*dV(q), dΩref) |> sum |> sqrt
         eh1 = ∫( q->∇eh(q)⋅∇eh(q)*dV(q), dΩref) |> sum |> sqrt
         results[:error_h1_norm] = eh1
@@ -96,7 +107,7 @@ function main_automatic(params)
     # Vtk output
     @timeit timer "vtk" if ! params[:export_vtu]
         gk.vtk_plot(params[:example_path],Ωref;refinement=4) do plt
-            gk.plot!(plt,q->u(ϕ(q));label="u")
+            gk.plot!(plt,q->u(ϕ_Ωref_Ω(q));label="u")
             gk.plot!(plt,uh;label="u")
         end
     end
@@ -134,15 +145,13 @@ function default_params()
     params[:timer] = TimerOutput()
     params[:mesh] = gk.cartesian_mesh((0,1,0,1),(10,10))
     params[:u] = (x) -> sum(x)
-    params[:f] = (x) -> 0.0
-    params[:g] = (x) -> 0.0
     params[:domain_tags] = ["interior"]
     params[:dirichlet_tags] = ["boundary"]
     params[:neumann_tags] = String[]
     params[:integration_degree] = 2
     params[:interpolation_degree] = 1
     params[:solver] = ps.lu_solver()
-    params[:example_path] = joinpath(mkpath("GalerkinToolkitExamples_output"),"example_001")
+    params[:example_path] = joinpath(mkpath(joinpath(@__DIR__,"..","output")),"example_001")
     params[:export_vtu] = true
     params
 end
