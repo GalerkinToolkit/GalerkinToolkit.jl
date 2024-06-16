fa7950795708c1cae93bc0a4bfce06ccb10229cc
Francesc Verdugo <f.verdugo.rojano@vu.nl>
2024-06-16 18:06:00 +0200

    Starting to add nietsche BCs

--- GalerkinToolkitExamples/src/example_001.jl
+++ GalerkinToolkitExamples/src/example_001.jl
@@ -29,92 +29,115 @@ function main_automatic(params)
     timer = params[:timer]
     results = Dict{Symbol,Any}()
 
-    # Geometry
-    @timeit timer "geometry" begin
-        mesh = params[:mesh]
-        D = gk.num_dims(mesh)
-        Ω = gk.domain(mesh,physical_names=params[:domain_tags])
-        Ωref = gk.reference_domain(Ω)
-        Γdiri = gk.domain(mesh;face_dim=D-1,physical_names=params[:dirichlet_tags])
-        Γ = gk.domain(mesh;face_dim=D-1,physical_names=params[:neumann_tags])
-        Γref = gk.reference_domain(Γ)
-    end
+    mesh = params[:mesh]
+    D = gk.num_dims(mesh)
+    Ω = gk.domain(mesh,physical_names=params[:domain_tags])
+    Ωr = gk.reference_domain(Ω)
+    Γd = gk.domain(mesh;face_dim=D-1,physical_names=params[:dirichlet_tags])
+    Γn = gk.domain(mesh;face_dim=D-1,physical_names=params[:neumann_tags])
+    Γnr = gk.reference_domain(Γn)
+    ϕ_Ωr_Ω = gk.domain_map(Ωr,Ω)
+    ϕ_Γnr_Γn = gk.domain_map(Γnr,Γn)
+    ϕ_Γnr_Ωr = gk.domain_map(Γnr,Ωr;face_around=1)
+    integration_degree = params[:integration_degree]
+    dΩr = gk.measure(Ωr,integration_degree)
+    dΓnr = gk.measure(Γnr,integration_degree)
 
-    # Geometrical map
-    ϕ_Ωref_Ω = gk.domain_map(Ωref,Ω)
-    ϕ_Γref_Γ = gk.domain_map(Γref,Γ)
-    ϕ_Γref_Ωref = gk.domain_map(Γref,Ωref;face_around=1)
+    u = gk.analytical_field(params[:u],Ω)
+    f(x) = -gk.call(Δ,u,x) # TODO
+    nn = gk.unit_normal(Γnr,Ω;face_around=1)
+    g(q) = nn(q)⋅ForwardDiff.gradient(u,ϕ_Γnr_Γn(q))
+
     function ∇(u,q)
-        J = ForwardDiff.jacobian(ϕ_Ωref_Ω,q)
+        J = ForwardDiff.jacobian(ϕ_Ωr_Ω,q)
         grad = ForwardDiff.gradient(u,q)
         J\grad
     end
     function dV(q)
-        J = ForwardDiff.jacobian(ϕ_Ωref_Ω,q)
+        J = ForwardDiff.jacobian(ϕ_Ωr_Ω,q)
         abs(det(J))
     end
-    function dS(q)
-        J = ForwardDiff.jacobian(ϕ_Γref_Γ,q)
+    function dSn(q)
+        J = ForwardDiff.jacobian(ϕ_Γnr_Γn,q)
         Jt = transpose(J)
         sqrt(det(Jt*J))
     end
     Δ(u,x) = tr(ForwardDiff.jacobian(y->ForwardDiff.gradient(u,y),x))
 
-    # Manufactured functions
-    u = gk.analytical_field(params[:u],Ω)
-    f(x) = -gk.call(Δ,u,x) # TODO
-    n = gk.unit_normal(Γref,Ω;face_around=1)
-    g(q) = n(q)⋅ForwardDiff.gradient(u,ϕ_Γref_Γ(q))
-
-    # Interpolation
-    @timeit timer "interpolation" begin
-        interpolation_degree = params[:interpolation_degree]
-        V = gk.lagrange_space(Ωref,interpolation_degree;dirichlet_boundary=Γdiri)
-        uh = gk.zero_field(Float64,V)
-        gk.interpolate_dirichlet!(u∘ϕ_Ωref_Ω,uh)
+    if params[:dirichlet_method] == :nitsche
+        Γdr = gk.reference_domain(Γd)
+        ϕ_Γdr_Γd = gk.domain_map(Γdr,Γd)
+        ϕ_Γdr_Ωr = gk.domain_map(Γdr,Ωr;face_around=1)
+        nd = gk.unit_normal(Γdr,Ω;face_around=1)
+        hd = gk.face_diameter_field(Γd)
+        dΓdr = gk.measure(Γdr,integration_degree)
+        γ = integration_degree*(integration_degree+1)
+        γ = γ/10.0
+        function dSd(q)
+            J = ForwardDiff.jacobian(ϕ_Γdr_Γd,q)
+            Jt = transpose(J)
+            sqrt(det(Jt*J))
+        end
     end
 
-    # Integration
-    integration_degree = params[:integration_degree]
-    dΩref = gk.measure(Ωref,integration_degree)
-    dΓref = gk.measure(Γref,integration_degree)
+    interpolation_degree = params[:interpolation_degree]
+    @assert params[:dirichlet_method] in (:strong,:nitsche)
+    if params[:dirichlet_method] == :nitsche
+        V = gk.lagrange_space(Ωr,interpolation_degree)
+    else
+        V = gk.lagrange_space(Ωr,interpolation_degree;dirichlet_boundary=Γd)
+    end
+    uh = gk.zero_field(Float64,V)
+    gk.interpolate_dirichlet!(u∘ϕ_Ωr_Ω,uh)
 
-    # Weak form
-    a(u,v) = ∫( q->∇(u,q)⋅∇(v,q)*dV(q), dΩref)
-    l(v) =
-        ∫( q->f(ϕ_Ωref_Ω(q))*v(q)*dV(q) , dΩref) +
-        ∫( q->g(q)*v(ϕ_Γref_Ωref(q))*dS(q) , dΓref)
+    function a(u,v)
+        r = ∫( q->∇(u,q)⋅∇(v,q)*dV(q), dΩr)
+        if params[:dirichlet_method] == :nitsche
+            r += ∫( q->
+                   (γ/hd(q))*v(ϕ_Γdr_Ωr(q))*u(ϕ_Γdr_Ωr(q))*dSd(q)-
+                   v(ϕ_Γdr_Ωr(q))*(nd(q)⋅∇(u,ϕ_Γdr_Ωr(q)))*dSd(q)-
+                   (nd(q)⋅∇(v,ϕ_Γdr_Ωr(q)))*u(ϕ_Γdr_Ωr(q))*dSd(q), dΓdr)
+        end
+        r
+    end
+    function l(v)
+        r =∫( q->f(ϕ_Ωr_Ω(q))*v(q)*dV(q) , dΩr) +
+        ∫( q->g(q)*v(ϕ_Γnr_Ωr(q))*dSn(q) , dΓnr)
+        if params[:dirichlet_method] == :nitsche
+            r += ∫( q->
+                (γ/hd(q))*v(ϕ_Γdr_Ωr(q))*u(ϕ_Γdr_Γd(q))*dSd(q)-
+                (nd(q)⋅∇(v,ϕ_Γdr_Ωr(q)))*u(ϕ_Γdr_Γd(q))*dSd(q), dΓdr)
+        end
+        r
+    end
 
-    # FE assembly
     @timeit timer "assembly" begin
+        # TODO give a hint when dirichlet BCS are homogeneous or not present
         x,A,b = gk.linear_problem(uh,a,l)
     end
 
-    # Linear solve
     @timeit timer "solver" begin
         P = ps.setup(params[:solver],x,A,b)
         ps.solve!(x,P,b)
     end
 
-    # Error norms
     @timeit timer "error_norms" begin
-        eh(q) = u(ϕ_Ωref_Ω(q)) - uh(q)
-        ∇eh(q) = ForwardDiff.gradient(u,ϕ_Ωref_Ω(q)) - ∇(uh,q)
-        el2 = ∫( q->abs2(eh(q))*dV(q), dΩref) |> sum |> sqrt
-        eh1 = ∫( q->∇eh(q)⋅∇eh(q)*dV(q), dΩref) |> sum |> sqrt
+        eh(q) = u(ϕ_Ωr_Ω(q)) - uh(q)
+        ∇eh(q) = ForwardDiff.gradient(u,ϕ_Ωr_Ω(q)) - ∇(uh,q)
+        el2 = ∫( q->abs2(eh(q))*dV(q), dΩr) |> sum |> sqrt
+        eh1 = ∫( q->∇eh(q)⋅∇eh(q)*dV(q), dΩr) |> sum |> sqrt
         results[:error_h1_norm] = eh1
         results[:error_l2_norm] = el2
     end
 
-    # Vtk output
     @timeit timer "vtk" if params[:export_vtu]
-        gk.vtk_plot(params[:example_path]*"_Ωref",Ωref;refinement=4) do plt
-            gk.plot!(plt,q->u(ϕ_Ωref_Ω(q));label="u")
-            gk.plot!(plt,q->f(ϕ_Ωref_Ω(q));label="f")
+        gk.vtk_plot(params[:example_path]*"_Ωr",Ωr;refinement=4) do plt
+            gk.plot!(plt,q->u(ϕ_Ωr_Ω(q));label="u")
+            gk.plot!(plt,q->f(ϕ_Ωr_Ω(q));label="f")
             gk.plot!(plt,uh;label="uh")
         end
-        gk.vtk_plot(params[:example_path]*"_Γref",Γref;refinement=4) do plt
-            gk.plot!(plt,n;label="n")
+        gk.vtk_plot(params[:example_path]*"_Γnr",Γnr;refinement=4) do plt
+            gk.plot!(plt,nn;label="n")
             gk.plot!(plt,g;label="g")
         end
     end
@@ -155,6 +178,7 @@ function default_params()
     params[:domain_tags] = ["interior"]
     params[:dirichlet_tags] = ["boundary"]
     params[:neumann_tags] = String[]
+    params[:dirichlet_method] = :strong
     params[:integration_degree] = 2
     params[:interpolation_degree] = 1
     params[:solver] = ps.lu_solver()
--- GalerkinToolkitExamples/test/example_001_tests.jl
+++ GalerkinToolkitExamples/test/example_001_tests.jl
@@ -6,14 +6,18 @@ using Test
 
 tol = 1.0e-10
 
-params = Dict{Symbol,Any}()
 n = 2
-params[:mesh] = gk.cartesian_mesh((0,2,0,2),(n,n))
-params[:dirichlet_tags] = ["1-face-1","1-face-3","1-face-4"]
-params[:neumann_tags] = ["1-face-2"]
-results = Example001.main(params)
-@test results[:error_h1_norm] < tol
-@test results[:error_l2_norm] < tol
+mesh = gk.cartesian_mesh((0,2,0,2),(n,n))
+for dirichlet_method in (:strong,)#:nitsche)
+    params = Dict{Symbol,Any}()
+    params[:mesh] = mesh
+    params[:dirichlet_tags] = ["1-face-1","1-face-3","1-face-4"]
+    params[:neumann_tags] = ["1-face-2"]
+    params[:dirichlet_method] = dirichlet_method
+    results = Example001.main(params)
+    @test results[:error_h1_norm] < tol
+    @test results[:error_l2_norm] < tol
+end
 
 end # module
 
