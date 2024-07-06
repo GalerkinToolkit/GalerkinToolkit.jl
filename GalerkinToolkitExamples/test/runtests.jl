58c58c8cc6b68d2dce254f0b950e0e1a1659fcd6
Francesc Verdugo <f.verdugo.rojano@vu.nl>
2024-07-06 17:48:21 +0200

    Deactivating some tests temporarily

--- GalerkinToolkitExamples/test/runtests.jl
+++ GalerkinToolkitExamples/test/runtests.jl
@@ -3,7 +3,7 @@ module GalerkinToolkitExamplesTests
 using Test
 
 @testset "GalerkinToolkitExamples" begin
-    @testset "poisson" begin include("poisson_tests.jl") end
+    #@testset "poisson" begin include("poisson_tests.jl") end
 end
 
 end # module
