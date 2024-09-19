88ec7cd24680c85adfc3ec12385bead9f8090577
Cmurilochem <carlosmurilorocha@gmail.com>
2024-09-19 14:26:54 +0200

    Add initial structure with Literate

--- docs/make.jl
+++ docs/make.jl
@@ -1,7 +1,17 @@
 using GalerkinToolkit
+using Literate
 using Documenter
 
 DocMeta.setdocmeta!(GalerkinToolkit, :DocTestSetup, :(using GalerkinToolkit); recursive=true)
+
+src_dir = joinpath(@__DIR__, "src")
+tutorials_dir = joinpath(@__DIR__, "src/tutorials")
+
+tutorials = ["poisson_tutorial.jl"]
+for tutorial in tutorials
+    tutorial_jl = joinpath(@__DIR__, tutorial)
+    Literate.markdown(tutorial_jl, tutorials_dir)
+end
 
 makedocs(;
     modules=[GalerkinToolkit],
@@ -14,6 +24,9 @@ makedocs(;
     ),
     pages=[
         "Home" => "index.md",
+        "Tutorials" =>[
+            "Poisson equation" => "tutorials/poisson_tutorial.md",
+        ],
         "Reference" =>[
                        "Geometry" => "reference/geometry.md",
                        "Integration" => "reference/integration.md",
---
+++ docs/poisson_tutorial.jl
@@ -0,0 +1,15 @@
+# In this tutorial, we will learn
+#
+#    -  How to solve a simple PDE in Julia with Gridap
+#    -  How to load a discrete model (aka a FE mesh) from a file
+#    -  How to build a conforming Lagrangian FE space
+#    -  How to define the different terms in a weak form
+#    -  How to impose Dirichlet and Neumann boundary conditions
+#    -  How to visualize results
+#
+#
+# ## Problem statement
+#
+# In this first tutorial, we provide an overview of a complete simulation pipeline in Gridap: from the construction of the FE mesh to the visualization of the computed results. To this end, we consider a simple model problem: the Poisson equation.
+#  We want to solve the Poisson equation on the 3D domain depicted in next figure with Dirichlet and Neumann boundary conditions. Dirichlet boundary conditions are applied on $\Gamma_{\rm D}$, being the outer sides of the prism (marked in red). Non-homogeneous Neumann conditions are applied to the internal boundaries $\Gamma_{\rm G}$, $\Gamma_{\rm Y}$, and $\Gamma_{\rm B}$ (marked in green, yellow and blue respectively). And homogeneous Neumann boundary conditions are applied in $\Gamma_{\rm W}$, the remaining portion of the boundary (marked in white).
+#
