8cd0c5194f64ee4c4503a0dcc255c58648846ab3
Francesc Verdugo <f.verdugo.rojano@vu.nl>
2024-10-20 08:11:14 +0200

    Adding skeleton of documentation

--- docs/make.jl
+++ docs/make.jl
@@ -3,15 +3,28 @@ using Documenter
 using Literate
 
 src_dir = joinpath(@__DIR__,"src") 
-examples_jl  = joinpath(src_dir,"examples.jl")
-Literate.markdown(examples_jl,src_dir)
+examples_dir = joinpath(src_dir,"examples") 
+examples = [
+            "problem_types",
+            "mesh_generation",
+            "interpolations",
+            "boundary_conditions",
+            "fields",
+            "solvers",
+            "posprocessing",
+            "visualization",
+           ]
+for example in examples
+    file_jl = joinpath(examples_dir,example*".jl")
+    Literate.markdown(file_jl,examples_dir)
+end
 
 DocMeta.setdocmeta!(GalerkinToolkit, :DocTestSetup, :(using GalerkinToolkit); recursive=true)
 
 makedocs(;
     modules=[GalerkinToolkit],
     authors="Francesc Verdugo <f.verdugo.rojano@vu.nl> and contributors",
-    sitename="GalerkinToolkit.jl",
+    sitename="GalerkinToolkit",
     format=Documenter.HTML(;
         prettyurls=get(ENV, "CI", "false") == "true",
         canonical="https://GalerkinToolkit.github.io/GalerkinToolkit.jl",
@@ -19,7 +32,7 @@ makedocs(;
     ),
     pages=[
         "Home" => "index.md",
-        "examples.md",
+        "Examples" => map(example->"examples/$(example).md",examples),
         "Manual" =>[
                        "Getting Started" => "manual/getting_started.md",
                       ],
