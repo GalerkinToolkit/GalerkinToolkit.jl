c065eeb379808e8d43ec5271414cf6d0f592ed18
raar1 <raarichardson@gmail.com>
2024-12-01 22:28:50 +0100

    Try adding the automatic poisson as a benchmark too

--- GalerkinToolkitExamples/benchmarks/run_benchmarks.jl
+++ GalerkinToolkitExamples/benchmarks/run_benchmarks.jl
@@ -7,7 +7,7 @@ using GalerkinToolkitExamples: Poisson
 
 
 function handwritten_poisson(n)
-	mesh = GT.cartesian_mesh((0,2,0,2,0,2),(n,n,n))
+	mesh = GT.cartesian_mesh((0,2,0,2,0,2), (n,n,n))
 
 	params = Dict{Symbol,Any}()
 	params[:implementation] = :hand_written
@@ -20,9 +20,26 @@ function handwritten_poisson(n)
 	Poisson.main(params)
 end
 
+function automatic_poisson(n)
+	mesh = GT.cartesian_mesh((0,2,0,2,0,2), (n,n,n))
+
+	params = Dict{Symbol,Any}()
+	params[:implementation] = :automatic
+	params[:mesh] = mesh
+	params[:dirichlet_tags] = ["1-face-1","1-face-2","1-face-3","1-face-4"]
+	params[:discretization_method] = :continuous_galerkin
+	params[:dirichlet_method] = :strong
+	params[:integration_degree] = 2
+
+	Poisson.main(params)
+end
+
 suite = BenchmarkGroup()
-suite["poisson"] = BenchmarkGroup(["Poisson", "handwritten"])
-suite["poisson"]["n=150"] = @benchmarkable handwritten_poisson(10)
+suite["poisson-hand"] = BenchmarkGroup(["Poisson", "handwritten"])
+suite["poisson-hand"]["n=10"] = @benchmarkable handwritten_poisson(10)
+
+suite["poisson-auto"] = BenchmarkGroup(["Poisson", "automatic"])
+suite["poisson-auto"]["n=10"] = @benchmarkable automatic_poisson(10)
 
 tune!(suite)
 
