155b1945aed02a6c6447de26a6eb8d39f6d41074
github-action-benchmark <github@users.noreply.github.com>
2024-12-02 15:47:47 +0000

    add Julia benchmark result (julia) benchmark result for f263dee95c252ba53490477578e5f43ea5f3b1da

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1733153113803,
+  "lastUpdate": 1733154467869,
   "repoUrl": "https://github.com/GalerkinToolkit/GalerkinToolkit.jl",
   "entries": {
     "Julia benchmark result": [
@@ -26,6 +26,32 @@ window.BENCHMARK_DATA = {
             "value": 7386530699,
             "unit": "ns",
             "extra": "gctime=437415119\nmemory=7481652776\nallocs=169412438\nparams={\"gctrial\":true,\"time_tolerance\":0.05,\"evals_set\":false,\"samples\":10000,\"evals\":1,\"gcsample\":false,\"seconds\":5,\"overhead\":0,\"memory_tolerance\":0.01}"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "GalerkinToolkit",
+            "username": "GalerkinToolkit"
+          },
+          "committer": {
+            "name": "GalerkinToolkit",
+            "username": "GalerkinToolkit"
+          },
+          "id": "f263dee95c252ba53490477578e5f43ea5f3b1da",
+          "message": "Use dedicated branch for benchmark results",
+          "timestamp": "2024-12-02T11:07:49Z",
+          "url": "https://github.com/GalerkinToolkit/GalerkinToolkit.jl/pull/151/commits/f263dee95c252ba53490477578e5f43ea5f3b1da"
+        },
+        "date": 1733154467605,
+        "tool": "julia",
+        "benches": [
+          {
+            "name": "poisson-hand/n=10",
+            "value": 7453266638,
+            "unit": "ns",
+            "extra": "gctime=462559924\nmemory=7481653080\nallocs=169412439\nparams={\"gctrial\":true,\"time_tolerance\":0.05,\"evals_set\":false,\"samples\":10000,\"evals\":1,\"gcsample\":false,\"seconds\":5,\"overhead\":0,\"memory_tolerance\":0.01}"
           }
         ]
       }
