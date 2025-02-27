451eba3d5c72fc720bcedc76a04a567960761981
Francesc Verdugo <f.verdugo.rojano@vu.nl>
2025-02-27 13:20:50 +0100

    Another attempt to fix tests

--- src/visualization.jl
+++ src/visualization.jl
@@ -742,36 +742,36 @@ function quadrature(plt::Plot)
     mesh_quadrature(;domain,face_reference_id,reference_quadratures)
 end
 
-function plot!(plt::Plot,field::DiscreteField;label)
-    q = GT.coordinates(plt)
-    f_q = field(q)
-    domain = GT.domain(plt)
-    index = GT.generate_index(domain)
-    t = term(f_q,index)
-    T = typeof(prototype(t))
-    x = quadrature(plt)
-    vmesh  = plt.mesh
-    nnodes = GT.num_nodes(vmesh)
-    data = zeros(T,nnodes)
-    nfaces = num_faces(domain)
-    face_point_u = discrete_field_accessor(GT.value,field,x)
-    face_point_J = jacobian_accessor(x)
-    face_npoints = num_points_accessor(x)
-    n = 0
-    for face in 1:nfaces
-        point_u = face_point_u(face)
-        point_J = face_point_J(face)
-        npoints = face_npoints(face)
-        for point in npoints
-            n += 1
-            J = point_J(point)
-            u = point_u(point,J)
-            data[n] = u
-        end
-    end
-    plt.node_data[label] = data
-    plt
-end
+#function plot!(plt::Plot,field::DiscreteField;label)
+#    q = GT.coordinates(plt)
+#    f_q = field(q)
+#    domain = GT.domain(plt)
+#    index = GT.generate_index(domain)
+#    t = term(f_q,index)
+#    T = typeof(prototype(t))
+#    x = quadrature(plt)
+#    vmesh  = plt.mesh
+#    nnodes = GT.num_nodes(vmesh)
+#    data = zeros(T,nnodes)
+#    nfaces = num_faces(domain)
+#    face_point_u = discrete_field_accessor(GT.value,field,x)
+#    face_point_J = jacobian_accessor(x)
+#    face_npoints = num_points_accessor(x)
+#    n = 0
+#    for face in 1:nfaces
+#        point_u = face_point_u(face)
+#        point_J = face_point_J(face)
+#        npoints = face_npoints(face)
+#        for point in npoints
+#            n += 1
+#            J = point_J(point)
+#            u = point_u(point,J)
+#            data[n] = u
+#        end
+#    end
+#    plt.node_data[label] = data
+#    plt
+#end
 
 domain(plt::Union{Plot,PPlot}) = plt.cache.domain
 
