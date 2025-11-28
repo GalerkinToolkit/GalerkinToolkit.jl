b8be6be265b66863a0e858fdd5495a70a42bc082
Francesc Verdugo <f.verdugo.rojano@vu.nl>
2025-11-28 16:30:39 +0100

    Fix docs

--- docs/make.jl
+++ docs/make.jl
@@ -1,5 +1,5 @@
 #using GalerkinToolkit
 #include("make_defs.jl")
 #Make.main(;debug=false)
-include("docs/make_media.jl")
-include("docs/make_with_media.jl")
+include("make_media.jl")
+include("make_with_media.jl")
